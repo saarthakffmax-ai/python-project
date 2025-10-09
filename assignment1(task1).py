73bb8207fd2468a50c317a5f97ffc1f8e04b1012
saarthakffmax-ai <saarthak.ff.max@gmail.com>
2025-10-09 21:40:29 +0530

    Add files via upload

---
+++ assignment1(task1).py
@@ -0,0 +1,3 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+print("addition", a + b, "substraction", a - b, "multiplication", a * b, "division", a / b)
---
+++ assignment1(task2).py
@@ -0,0 +1,3 @@
+a=input("enter your first name")
+b=input("enter another last name")
+print("hello",a, b,"nice to meet you!")
